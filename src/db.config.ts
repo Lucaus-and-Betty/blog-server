18fc3004b879e901f79c1e99010065cf635585d0
betty <wusehun94zlj@gmail.com>
2024-11-20 13:07:38 +0800

    feat: ✨ 添加环境变量

--- src/db.config.ts
+++ src/db.config.ts
@@ -1,13 +1,22 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+if (process.env.RUNNING_ENV !== 'prod') {
+  dotenv.config({ path: `.env.dev` });
+} else {
+  dotenv.config({ path: '.env.prod' });
+}
 
 export const DB_CONFIG: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '123456',
+  host: process.env.NODE_ENV_DB_HOST,
+  port: Number(process.env.NODE_ENV_DB_PORT),
+  username: process.env.NODE_ENV_DB_USER,
+  password: process.env.NODE_ENV_DB_PASSWORD,
   database: 'blog_db',
   autoLoadEntities: true,
   synchronize: false,
   entities: []
 };
+
+console.log(DB_CONFIG);
