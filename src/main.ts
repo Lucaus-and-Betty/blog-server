672f9e4a9763830028a820783518bf331579fc18
betty <wusehun94zlj@gmail.com>
2024-12-06 13:05:50 +0800

    feat: ✨ 上线

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
   app.enableCors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: 'http://116.196.66.106',
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type, Accept'
   });
